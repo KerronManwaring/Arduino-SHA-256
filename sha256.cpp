b4b722e67de6c6f35ab358493e676971f96e32de
Simon Ratner <simon+github@probablyprime.net>
2016-02-17 21:06:28 -0800

    Clean up, add eeprom support and reset, remove static instance

--- sha256.cpp
+++ sha256.cpp
@@ -1,9 +1,11 @@
 #include <string.h>
 #include <avr/io.h>
+#include <avr/eeprom.h>
 #include <avr/pgmspace.h>
+
 #include "sha256.h"
 
-const uint32_t sha256K[] PROGMEM = {
+const uint32_t SHA256_K[] PROGMEM = {
   0x428a2f98,0x71374491,0xb5c0fbcf,0xe9b5dba5,0x3956c25b,0x59f111f1,0x923f82a4,0xab1c5ed5,
   0xd807aa98,0x12835b01,0x243185be,0x550c7dc3,0x72be5d74,0x80deb1fe,0x9bdc06a7,0xc19bf174,
   0xe49b69c1,0xefbe4786,0x0fc19dc6,0x240ca1cc,0x2de92c6f,0x4a7484aa,0x5cb0a9dc,0x76f988da,
@@ -14,9 +16,7 @@ const uint32_t sha256K[] PROGMEM = {
   0x748f82ee,0x78a5636f,0x84c87814,0x8cc70208,0x90befffa,0xa4506ceb,0xbef9a3f7,0xc67178f2
 };
 
-#define BUFFER_SIZE 64
-
-const uint8_t sha256InitState[] PROGMEM = {
+const uint8_t SHA256_INIT_STATE[] PROGMEM = {
   0x67,0xe6,0x09,0x6a, // H0
   0x85,0xae,0x67,0xbb, // H1
   0x72,0xf3,0x6e,0x3c, // H2
@@ -27,17 +27,15 @@ const uint8_t sha256InitState[] PROGMEM = {
   0x19,0xcd,0xe0,0x5b  // H7
 };
 
-void Sha256Class::init(void) {
-  memcpy_P(state.b,sha256InitState,32);
+#define ror32(num, bits) ((num << (32 - bits)) | (num >> bits))
+
+void Sha256::init(void) {
+  memcpy_P(state.b, SHA256_INIT_STATE, 32);
   byteCount = 0;
   bufferOffset = 0;
 }
 
-uint32_t Sha256Class::ror32(uint32_t number, uint8_t bits) {
-  return ((number << (32-bits)) | (number >> bits));
-}
-
-void Sha256Class::hashBlock() {
+void Sha256::hashBlock() {
   uint8_t i;
   uint32_t a,b,c,d,e,f,g,h,t1,t2;
 
@@ -62,7 +60,7 @@ void Sha256Class::hashBlock() {
     t1 = h;
     t1 += ror32(e,6) ^ ror32(e,11) ^ ror32(e,25); // ∑1(e)
     t1 += g ^ (e & (g ^ f)); // Ch(e,f,g)
-    t1 += pgm_read_dword(sha256K+i); // Ki
+    t1 += pgm_read_dword(SHA256_K + i); // Ki
     t1 += buffer.w[i&15]; // Wi
     t2 = ror32(a,2) ^ ror32(a,13) ^ ror32(a,22); // ∑0(a)
     t2 += ((b & c) | (a & (b | c))); // Maj(a,b,c)
@@ -78,53 +76,52 @@ void Sha256Class::hashBlock() {
   state.w[7] += h;
 }
 
-void Sha256Class::addUncounted(uint8_t data) {
+void Sha256::push(uint8_t data) {
   buffer.b[bufferOffset ^ 3] = data;
   bufferOffset++;
-  if (bufferOffset == BUFFER_SIZE) {
+  if (bufferOffset == BLOCK_LENGTH) {
     hashBlock();
     bufferOffset = 0;
   }
 }
 
 #if defined(ARDUINO) && ARDUINO >= 100
-size_t
+size_t Sha256::write(uint8_t data) {
 #else
-void
+void Sha256::write(uint8_t data) {
 #endif
-Sha256Class::write(uint8_t data) {
   ++byteCount;
-  addUncounted(data);
+  push(data);
 #if defined(ARDUINO) && ARDUINO >= 100
   return 1;
 #endif
 }
 
-void Sha256Class::pad() {
+void Sha256::padBlock() {
   // Implement SHA-256 padding (fips180-2 §5.1.1)
 
   // Pad with 0x80 followed by 0x00 until the end of the block
-  addUncounted(0x80);
-  while (bufferOffset != 56) addUncounted(0x00);
+  push(0x80);
+  while (bufferOffset != 56) push(0x00);
 
-  // Append length in the last 8 bytes
-  addUncounted(0); // We're only using 32 bit lengths
-  addUncounted(0); // But SHA-1 supports 64 bit lengths
-  addUncounted(0); // So zero pad the top bits
-  addUncounted(byteCount >> 29); // Shifting to multiply by 8
-  addUncounted(byteCount >> 21); // as SHA-1 supports bitstreams as well as
-  addUncounted(byteCount >> 13); // byte.
-  addUncounted(byteCount >> 5);
-  addUncounted(byteCount << 3);
+  // Append length in the last 8 bytes. We're only using 32 bit lengths, but
+  // SHA-2 supports 64 bit lengths so zero pad the top bits
+  push(0);
+  push(0);
+  push(0);
+  push(byteCount >> 29);
+  push(byteCount >> 21);
+  push(byteCount >> 13);
+  push(byteCount >> 5);
+  push(byteCount << 3);
 }
 
-
-uint8_t* Sha256Class::result(void) {
+uint8_t* Sha256::result(void) {
   // Pad to complete the last block
-  pad();
+  padBlock();
 
   // Swap byte order back
-  for (int i=0; i<8; i++) {
+  for (uint8_t i = 0; i < 8; i++) {
     uint32_t a,b;
     a=state.w[i];
     b=a<<24;
@@ -134,43 +131,56 @@ uint8_t* Sha256Class::result(void) {
     state.w[i]=b;
   }
 
-  // Return pointer to hash (20 characters)
+  // Return pointer to hash
   return state.b;
 }
 
 #define HMAC_IPAD 0x36
 #define HMAC_OPAD 0x5c
 
-uint8_t keyBuffer[BLOCK_LENGTH]; // K0 in FIPS-198a
-uint8_t innerHash[HASH_LENGTH];
-
-void Sha256Class::initHmac(const uint8_t* key, int keyLength) {
-  uint8_t i;
-  memset(keyBuffer,0,BLOCK_LENGTH);
+void Sha256::initHmac(const uint8_t *key, size_t keyLength) {
+  memset(keyBuffer, 0, BLOCK_LENGTH);
   if (keyLength > BLOCK_LENGTH) {
     // Hash long keys
     init();
     for (;keyLength--;) write(*key++);
-    memcpy(keyBuffer,result(),HASH_LENGTH);
+    memcpy(keyBuffer, result(), HASH_LENGTH);
   } else {
     // Block length keys are used as is
-    memcpy(keyBuffer,key,keyLength);
+    memcpy(keyBuffer, key, keyLength);
   }
+  reset();
+}
+
+void Sha256::initHmac_EEPROM(const uint8_t *key, size_t keyLength) {
+  memset(keyBuffer, 0, BLOCK_LENGTH);
+  if (keyLength > BLOCK_LENGTH) {
+    // Hash long keys
+    init();
+    for (;keyLength--;) write(eeprom_read_byte(key++));
+    memcpy(keyBuffer, result(), HASH_LENGTH);
+  } else {
+    // Block length keys are used as is
+    eeprom_read_block(keyBuffer, key, keyLength);
+  }
+  reset();
+}
+
+uint8_t* Sha256::resultHmac(void) {
+  uint8_t i;
+  // Complete inner hash
+  memcpy(innerHash, result(), HASH_LENGTH);
+  // Calculate outer hash
+  init();
+  for (i = 0; i < BLOCK_LENGTH; i++) write(keyBuffer[i] ^ HMAC_OPAD);
+  for (i = 0; i < HASH_LENGTH; i++) write(innerHash[i]);
+  return result();
+}
+
+void Sha256::reset(void) {
   // Start inner hash
   init();
-  for (i=0; i<BLOCK_LENGTH; i++) {
+  for (uint8_t i = 0; i < BLOCK_LENGTH; i++) {
     write(keyBuffer[i] ^ HMAC_IPAD);
   }
 }
-
-uint8_t* Sha256Class::resultHmac(void) {
-  uint8_t i;
-  // Complete inner hash
-  memcpy(innerHash,result(),HASH_LENGTH);
-  // Calculate outer hash
-  init();
-  for (i=0; i<BLOCK_LENGTH; i++) write(keyBuffer[i] ^ HMAC_OPAD);
-  for (i=0; i<HASH_LENGTH; i++) write(innerHash[i]);
-  return result();
-}
-Sha256Class Sha256;
--- sha256.h
+++ sha256.h
@@ -7,20 +7,26 @@
 #define HASH_LENGTH 32
 #define BLOCK_LENGTH 64
 
-union _buffer {
-  uint8_t b[BLOCK_LENGTH];
-  uint32_t w[BLOCK_LENGTH/4];
-};
-union _state {
-  uint8_t b[HASH_LENGTH];
-  uint32_t w[HASH_LENGTH/4];
-};
+class Sha256 : public Print {
 
-class Sha256Class : public Print
-{
+  union Buffer {
+    uint8_t b[BLOCK_LENGTH];
+    uint32_t w[BLOCK_LENGTH / 4];
+  };
+
+  union State {
+    uint8_t b[HASH_LENGTH];
+    uint32_t w[HASH_LENGTH / 4];
+  };
+
   public:
     void init(void);
-    void initHmac(const uint8_t* secret, int secretLength);
+    void initHmac(const uint8_t *key, size_t keyLength);
+    void initHmac_EEPROM(const uint8_t *key, size_t keyLength);
+
+    // Reset to initial state, but preserve key material.
+    void reset(void);
+
     uint8_t* result(void);
     uint8_t* resultHmac(void);
 #if defined(ARDUINO) && ARDUINO >= 100
@@ -29,18 +35,20 @@ class Sha256Class : public Print
     virtual void write(uint8_t);
 #endif
     using Print::write;
+
   private:
-    void pad();
-    void addUncounted(uint8_t data);
     void hashBlock();
-    uint32_t ror32(uint32_t number, uint8_t bits);
-    _buffer buffer;
-    uint8_t bufferOffset;
-    _state state;
+    void padBlock();
+    void push(uint8_t data);
+
     uint32_t byteCount;
+
     uint8_t keyBuffer[BLOCK_LENGTH];
     uint8_t innerHash[HASH_LENGTH];
+
+    State state;
+    Buffer buffer;
+    uint8_t bufferOffset;
 };
-extern Sha256Class Sha256;
 
 #endif
